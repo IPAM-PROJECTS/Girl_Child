a3c8cd1ef9efde66c5c2754e4266df62a0e4951a
sidiksam <samuraabubakarr03@gmail.com>
2023-02-28 23:39:54 -0800

    Work on the website responsive

--- js/script.js
+++ js/script.js
@@ -1,56 +1,60 @@
 const speakers = {
   speaker1: {
     id: 1,
-    name: 'Fawaz Milton',
-    title: 'CEO and Director of Girl Child Education Organization',
-    about: 'Fawaz discovered The Girl pinin 4 beteh organization in 2015. He studied Applied Accounting in the Institute of Public Administration and Management(IPAM) University.',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/fawaz.jpg',
+    name: "Fawaz Milton",
+    title: "CEO and Director of Girl Child Education Organization",
+    about:
+      "Fawaz discovered The Girl pinin 4 beteh organization in 2015. He studied Applied Accounting in the Institute of Public Administration and Management(IPAM) University.",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/fawaz.jpg",
   },
   speaker2: {
     id: 2,
-    name: 'Fatima Bio',
-    title: 'Founder of the women free education',
-    about: 'She has a Bachelor of Arts degree in journalism at the University of the Arts, London College of Communication,',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/fatima.jpg',
+    name: "Fatima Bio",
+    title: "Founder of the women free education",
+    about:
+      "She has a Bachelor of Arts degree in journalism at the University of the Arts, London College of Communication,",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/fatima.jpg",
   },
   speaker3: {
     id: 3,
-    name: 'Santos Kamara',
-    title: 'Founder and CEO of My Mother Choice Foundation',
-    about: 'Student at Institute of Public Administration and Management(IPAM) University studying Applied Accounting ',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/santos.jpg',
+    name: "Santos Kamara",
+    title: "Founder and CEO of My Mother Choice Foundation",
+    about:
+      "Student at Institute of Public Administration and Management(IPAM) University studying Applied Accounting ",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/santos.jpg",
   },
   speaker4: {
     id: 4,
-    name: 'Sunkray Sunkarie',
-    title: 'Founder and the CEO of the Sunkarie Foundation',
-    about: 'She is a student of Frobay College (FBC)',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/sunkari-foundation_n.jpg',
+    name: "Sunkray Sunkarie",
+    title: "Founder and the CEO of the Sunkarie Foundation",
+    about: "She is a student of Frobay College (FBC) she has a charity foundation to sponsor the less privilege to go school.",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/sunkF.jpg",
   },
   speaker5: {
     id: 5,
-    name: 'Idrissa Bangura ',
-    title: 'Co-founder. of the Kids happiness foundation',
-    about: 'Studied social works in University of Milton margai. Currently the CEO of kids happiness foundation in Sierra Leone .',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/alabama.jpg',
+    name: "Idrissa Bangura ",
+    title: "Co-founder. of the Kids happiness foundation",
+    about:
+      "Studied social works in University of Milton margai. Currently the CEO of kids happiness foundation in Sierra Leone .",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/alabama.jpg",
   },
   speaker6: {
     id: 6,
-    name: 'Saibatu Mansaray',
-    title: 'Founder and CEO. The Saibatu Mansaray Journey',
-    about: 'A retired milliatery woman in the united state of america (USA).',
-    checkersImage: '../images/checkers.png',
-    featuredImage: '../images/saibatu-foundation.jpg',
+    name: "Saibatu Mansaray",
+    title: "Founder and CEO. The Saibatu Mansaray Journey",
+    about:
+      "Saibatu mansaray. is a former White House senior executive, a physician assistant, CEO and Founder of The Mansaray Foundation, a Muslimah.    ",
+    checkersImage: "../images/checkers.png",
+    featuredImage: "../images/saibatu.jpg",
   },
-
 };
 
-const SpeakerSection = document.querySelector('.speakersWrap');
+const SpeakerSection = document.querySelector(".speakersWrap");
 
 Object.keys(speakers).forEach((speaker) => {
   if (Object.hasOwnProperty.call(speakers, speaker)) {
@@ -72,18 +76,18 @@ Object.keys(speakers).forEach((speaker) => {
   }
 });
 
-const HamburgerMenu = document.querySelector('.hamburger');
-const MobileNav = document.querySelector('.mobileLink');
-const MobileNavLists = document.querySelectorAll('.nav-item-mobile');
+const HamburgerMenu = document.querySelector(".hamburger");
+const MobileNav = document.querySelector(".mobileLink");
+const MobileNavLists = document.querySelectorAll(".nav-item-mobile");
 
-HamburgerMenu.addEventListener('click', () => {
-  HamburgerMenu.classList.toggle('is-active');
-  MobileNav.classList.toggle('is-active');
+HamburgerMenu.addEventListener("click", () => {
+  HamburgerMenu.classList.toggle("is-active");
+  MobileNav.classList.toggle("is-active");
 });
 
 MobileNavLists.forEach((link) => {
-  link.addEventListener('click', () => {
-    HamburgerMenu.classList.toggle('is-active');
-    MobileNav.classList.toggle('is-active');
+  link.addEventListener("click", () => {
+    HamburgerMenu.classList.toggle("is-active");
+    MobileNav.classList.toggle("is-active");
   });
 });
